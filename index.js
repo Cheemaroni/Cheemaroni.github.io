02b981e9c450ef99e9d68488d29102fa2306be1a
Cheemaroni <102878517+Cheemaroni@users.noreply.github.com>
2023-09-09 11:58:46 -0400

    media player

---
+++ index.js
@@ -0,0 +1,14 @@
+const videoPlayer = document.getElementById('videoPlayer');
+
+// Add event listeners for player controls (optional)
+videoPlayer.addEventListener('play', () => {
+    // Do something when the video starts playing
+});
+
+videoPlayer.addEventListener('pause', () => {
+    // Do something when the video is paused
+});
+
+videoPlayer.addEventListener('ended', () => {
+    // Do something when the video ends
+});
