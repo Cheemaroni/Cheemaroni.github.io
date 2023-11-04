bc886199b80c24625c475d5f9b90a7ed84b80679
Cheemaroni <102878517+Cheemaroni@users.noreply.github.com>
2023-11-04 16:46:25 -0400

    Update myroom.js

--- myroom.js
+++ myroom.js
@@ -15,6 +15,6 @@ function startGame() {
 
         setTimeout(function () {
             gameScreen.style.backgroundImage = "url('wideroom.jpg')";
-        }, 5000); // Transition to the room image after 5 seconds
-    }, 5000); // Play door close audio after 5 seconds
+        }, 5000); 
+    }, 5000); 
 }
