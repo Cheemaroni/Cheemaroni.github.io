19ad2aa90ec4ba4bf349ec482b8dbbf376676b0d
Cheemaroni <102878517+Cheemaroni@users.noreply.github.com>
2023-11-04 15:56:43 -0400

    fucmking kill me

--- myroom.js
+++ myroom.js
@@ -1,47 +1,21 @@
-// Define game states
-const GameState = {
-    START: "start",
-    INSIDE_ROOM: "inside_room",
-};
-let gameState = GameState.START;
+const doorOpenAudio = document.getElementById("door-open-audio");
+const doorCloseAudio = document.getElementById("door-close-audio");
+const startScreen = document.getElementById("start-screen");
+const gameScreen = document.getElementById("game-screen");
+const textOverlay = document.getElementById("text-overlay");
 
 function startGame() {
-  console.log("Start button clicked.");
-    document.getElementById("start-button").style.display = "none";
-
-    var canvas = document.getElementById("game-canvas");
-    canvas.style.display = "block";
-    canvas.style.backgroundImage = "url('frontdoor.jpg')"; // This line sets the background image to the door image
-
-    var doorOpenAudio = document.getElementById("door-open-audio");
     doorOpenAudio.play();
-    
-    // Change the game state to INSIDE_ROOM after the door opening sound finishes
-    doorOpenAudio.addEventListener("ended", function() {
-        gameState = GameState.INSIDE_ROOM;
-    });
-}
+    startScreen.style.display = "none";
+    gameScreen.style.display = "block";
 
-function updateGame() {
-    var canvas = document.getElementById("game-canvas");
-    var context = canvas.getContext("2d");
-    
-    if (gameState === GameState.INSIDE_ROOM) {
-        var roomImage = new Image();
-        roomImage.src = "widroom.jpg";
-        roomImage.onload = function() {
-            context.drawImage(roomImage, 0, 0, canvas.width, canvas.height);
-        }
-        
-        context.fillStyle = "white";
-        context.font = "20px Arial";
-        context.fillText("You are in my room. I'm not here, but you still sense you aren't alone.", 50, 450);
-    }
+    setTimeout(function() {
+        doorOpenAudio.pause();
+        setTimeout(function() {
+            gameScreen.style.backgroundColor = "black";
+            doorCloseAudio.play();
+            gameScreen.style.backgroundImage = "url('widroom.jpg')";
+            textOverlay.style.display = "block";
+        }, 5000);
+    }, 5000);
 }
-
-function gameLoop() {
-    updateGame();
-    requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
