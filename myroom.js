30c46be4ab17485d63687f73a2b3056c4c3e0fd1
Cheemaroni <102878517+Cheemaroni@users.noreply.github.com>
2023-11-04 21:00:54 -0400

    my room update
    added text

--- myroom.js
+++ myroom.js
@@ -2,6 +2,8 @@ const startScreen = document.getElementById("start-screen");
 const gameScreen = document.getElementById("game-screen");
 const doorOpenAudio = document.getElementById("door-open-audio");
 const doorCloseAudio = document.getElementById("door-close-audio");
+const textContainer = document.getElementById("text");
+const text = "You are in my room. I'm not here, but you still sense you aren't alone.";
 
 function startGame() {
     startScreen.style.display = "none";
@@ -14,7 +16,19 @@ function startGame() {
         doorCloseAudio.play();
 
         setTimeout(function () {
-            gameScreen.style.backgroundImage = "url('Cheemaroni.github.io/wideroom.jpg')";
-        }, 5000); 
-    }, 5000); 
+            gameScreen.style.backgroundImage = "url('wideroom.jpg')";
+            textContainer.style.display = "block";
+            displayText();
+        }, 5000); // Transition to the room image after 5 seconds
+    }, 5000); // Play door close audio after 5 seconds
+}
+function displayText() {
+    let index = 0;
+    const interval = setInterval(function () {
+        textContainer.innerHTML += text[index];
+        index++;
+        if (index >= text.length) {
+            clearInterval(interval);
+        }
+    }, 100);
 }
