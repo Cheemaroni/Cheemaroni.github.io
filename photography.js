92a4fbecd4ec729af73dbb0217b4ed7df7bfa4f8
Cheemaroni <102878517+Cheemaroni@users.noreply.github.com>
2023-11-07 15:05:16 -0500

    added photography.html
    added also images for the photography

---
+++ photography.js
@@ -0,0 +1,20 @@
+function openModal(imageSrc) {
+    var modal = document.getElementById("myModal");
+    var modalImage = document.getElementById("modalImage");
+  
+    modal.style.display = "block";
+    modalImage.src = imageSrc;
+  }
+  
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  }
+  
+  window.onclick = function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+      closeModal();
+    }
+  }
+  
